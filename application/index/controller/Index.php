913baa007aebf84f74f678061dedbe26db3cfbb8
gtt2352 <2352836328@qq.com>
2018-11-28 18:00:44 +0800

    ss

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -32,11 +32,8 @@ class Index extends Base
 	public function single() 
 	{
 		// dump(input('.'));
-		 $tmp = file_get_contents("php://input");
-         $arr = json_decode($tmp,true);
-         $tid = $arr['id'];
-         echo $tid;
-         var_dump($arr);
+		$tmp = file_get_contents("php://input");        
+        	var_dump($tmp);
 		
 		$list = Db::name('list')->where(['id'=>input('post.id')])->select();
 		exit;
